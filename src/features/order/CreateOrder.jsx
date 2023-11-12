e9fc385d504058d665f1fa3f9d209f2428c68803
Haohe Zhu <2452466220@qq.com>
2023-11-12 09:56:18 +0800

    set username by redux

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
 import { createOrder } from '../../services/apiRestaurant';
 import Button from '../../ui/Button';
+import { useSelector } from 'react-redux';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -39,6 +40,7 @@ function CreateOrder() {
   const navigation = useNavigation();
   const isSubmitting = navigation.state === 'submitting';
   const formErrors = useActionData();
+  const username = useSelector((store) => store.user.username);
 
   return (
     <div className="px-4 py-6">
@@ -50,7 +52,13 @@ function CreateOrder() {
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">First Name</label>
           <div className="grow">
-            <input type="text" name="customer" required className="input" />
+            <input
+              type="text"
+              name="customer"
+              required
+              className="input"
+              defaultValue={username}
+            />
           </div>
         </div>
 
--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,5 +1,9 @@
+import { useSelector } from 'react-redux';
 import CreateUser from '../features/user/CreateUser';
+import Button from './Button';
 function Home() {
+  const username = useSelector((store) => store.user.username);
+
   return (
     <div className="my-10 px-4 text-center sm:my-16">
       <h1 className="mb-8 text-xl font-semibold md:text-3xl">
@@ -10,7 +14,11 @@ function Home() {
         </span>
       </h1>
 
-      <CreateUser />
+      {username === '' ? (
+        <CreateUser />
+      ) : (
+        <Button to="/menu">Continue ordering, {username}</Button>
+      )}
     </div>
   );
 }
